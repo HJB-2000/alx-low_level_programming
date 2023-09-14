26f7bdcbd594d7add903009a914c4d8dcb8d87ec
Jassim Bahmida <jassim123bahmida@gmail.com>
2023-09-14 15:24:35 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,83 +1,46 @@
 #include <stdio.h>
-#include "main.h"
+#include <math.h>
+
 /**
- * main - prints the biggest prime factor of a number.
+ * largest_prime_factor - Find the largest prime factor of a number
+ * @n: The number to find the largest prime factor of
  *
- * Return: Always 0.
+ * Return: The largest prime factor of n
  */
+long largest_prime_factor(long n)
+{
+	long largestFactor = -1;
+	long i;
+
+	while (n % 2 == 0)
+	{
+		largestFactor = 2;
+		n = n / 2;
+	}
+
+	for (i = 3; i <= sqrt(n); i += 2)
+	{
+		while (n % i == 0)
+		{
+			largestFactor = i;
+			n = n / i;
+		}
+	}
+
+	if (n > 2)
+	{
+		largestFactor = n;
+	}
+
+	return (largestFactor);
+}
+
 int main(void)
 {
-	long int number;
+	long number = 612852475143;
+	long largestPrime = largest_prime_factor(number);
 
-	number = 612852475143;
+	printf("%ld\n", largestPrime);
 
-	if (isPrime(number) == 1)
-	{
-		printf("%ld\n", number);
-	}
-	else
-	{
-		printf("%ld\n", biggestFactor(number));
-	}
 	return (0);
-}
-/**
- * isPrime - analise if a number is prime or not
- * @n: number to check
- * Return: true if it is prime false if not
- */
-int isPrime(long int n)
-{
-	int i;
-
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else if (n == 2)
-	{
-		return (1);
-	}
-	else
-	{
-		for (i = 2; i < n; i++)
-		{
-			if (n % i == 0)
-			{
-				return (0);
-			}
-		}
-	return (1);
-	}
-}
-/**
- * biggestFactor - returns the biggest prime factor of a number
- * @a: number to check
- * Return: biggest factor
- */
-long int biggestFactor(long int a)
-{
-	long int i, factor;
-
-	factor = a;
-	for (i = 2; i <= factor; i++)
-	{
-		if (isPrime(factor) == 1)
-		{
-			break;
-		}
-		else
-		{
-			if ((factor % i == 0) && (isPrime(i) == 1))
-			{
-				factor = factor / i;
-				continue;
-			}
-			else
-			{
-				factor = factor;
-			}
-		}
-	}
-	return (factor);
 }
