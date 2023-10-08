15ea0bc28373b8e19e1d3037e439118079fa5d4d
Jassim Bahmida <jassim123bahmida@gmail.com>
2023-10-07 18:35:17 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,51 +2,73 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/**
+ * isAllDigits - checks if a string consists of digits.
+ * @str: Pointer to the string to be checked.
+ *
+ * Return: 1 if not all digits, 0 if all digits.
+ */
+int isAllDigits(char *str)
+{
+	int i = 0;
 
+	while (str[i])
+	{
+		if (!isdigit(str[i]))
+			return 1;
+		i++;
+	}
+	return 0;
+}
 
 /**
- * main - Entry point
- * @argc: Number of arguments passed to the program
- * @argv: Array of pointers to strings containing the arguments
+ * customAtoi - converts a string of ASCII digits to an integer.
+ * @s: Pointer to the source string.
  *
- * Return: 0 on success, 98 on failure
- *
- * Description: This program multiplies two positive numbers and
- *  performs input validation and argument validation.
- * If the input is invalid, it prints an error message and
- *  exits with a status of 98. If the input is valid, it multiplies
- * the two numbers and prints the result followed by a new line.
+ * Return: Integer value of the digits.
  */
-
-
-
-int main(int argc, char **argv)
+int customAtoi(char *s)
 {
-	int i, j;
-	int num1 = 0, num2 = 0, result = 0;
+	int i = 0;
+	int result = 0;
+
+	while (s[i])
+	{
+		if (isdigit(s[i]))
+		{
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		i++;
+	}
+	return result;
+}
+
+/**
+ * main - entry point of the program.
+ * @argc: Argument count.
+ * @argv: 2D array of arguments.
+ *
+ * Return: 0 on success, 98 on failure.
+ */
+int main(int argc, char *argv[])
+{
+	int result;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return (98);
+		return 98;
 	}
 
-	for (i = 1; i < argc; i++)
+	if (isAllDigits(argv[1]) || isAllDigits(argv[2]))
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-		if (!isdigit(argv[i][j]))
-		{
-			printf("Error\n");
-			return (98);
-		}
-		}
+		printf("Error\n");
+		return 98;
 	}
 
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = num1 * num2;
-
+	result = customAtoi(argv[1]) * customAtoi(argv[2]);
 	printf("%d\n", result);
-	return (0);
+
+	return 0;
 }
