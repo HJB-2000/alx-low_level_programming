74c00698f78ff28a7ead7975aebf785d85247d78
Jassim Bahmida <jassim123bahmida@gmail.com>
2023-10-10 13:30:50 -0700

    0x0D-preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    printf("%s\n", __FILE__);
-    return 0;
+
+
+/**
+ * main - Unveils the mystical secrets
+ *        of its own existence by revealing
+ *        the name of its birthplace.
+ *
+ * Return: None - for it is the eternal wanderer
+ *         of the digital realm, with no return
+ *         to its source.
+ */
+
+
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
