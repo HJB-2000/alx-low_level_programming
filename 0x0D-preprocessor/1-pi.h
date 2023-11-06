1ec82760ff0cfd95e8cd6f90a5a4f8fb1f79885a
jassim bahmida <jassim123bahmida@gmail.com>
2023-11-06 14:50:20 -0800

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,16 @@
+#ifndef MY_CONSTANTS_PI_H
+#define MY_CONSTANTS_PI_H
+
+/*
+ * my_constants_pi.h
+ *
+ * Description:
+ * This header file defines a macro named PI as an abbreviation
+ *   for the value of π (approximately 3.14159265359).
+ * It allows you to use PI in your C code to represent
+ *   the mathematical constant π.
+ */
+
+#define PI 3.14159265359
+
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+/**
+ * main - Unveils the mystical secrets
+ *        of its own existence by revealing
+ *        the name of its birthplace.
+ *
+ * Return: None - for it is the eternal wanderer
+ *         of the digital realm, with no return
+ *         to its source.
+ */
+
+
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
