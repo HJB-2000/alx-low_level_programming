42ecdfaa9fdf83724461ff422e1be3eb6da0daa4
jassim bahmida <jassim123bahmida@gmail.com>
2023-11-16 18:00:38 -0800

    2

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * hhh - Print  string before the main .
+ */
+void __attribute__((constructor)) hhh(void)
+void hhh(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
