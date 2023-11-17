c134b65af42579861dfa7fb87f2d618076da6be0
jassim bahmida <jassim123bahmida@gmail.com>
2023-11-16 18:04:16 -0800

    3

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,8 +2,8 @@
 /**
  * hh - Print  string before the main .
  */
-void __attribute__((constructor)) hh(void)
-void hh(void)
+void __attribute__((constructor)) hare(void)
+void hare(void)
 {
 printf("You're beat! and yet, you must allow,\n"
 "I bore my house upon my back!\n");
