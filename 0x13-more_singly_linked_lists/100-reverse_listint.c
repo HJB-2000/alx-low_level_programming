0daacadc8f598047b8e523067ec44d38042e5fc7
jassim bahmida <jassim123bahmida@gmail.com>
2023-11-21 17:44:43 -0800

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,7 +11,7 @@
  * Return: A pointer to the first node of the reversed list.
  */
 
-listint_t *reverse_listint(listint_t **head) 
+listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *ahead, *behind, *current;
 
@@ -22,7 +22,7 @@ listint_t *reverse_listint(listint_t **head)
 	behind = NULL;
 	current = *head;
 
-	for (; current->next != NULL; *head = current, current = ahead) 
+	for (; current->next != NULL; *head = current, current = ahead)
 	{
 		ahead = current->next;
 		current->next = behind;
