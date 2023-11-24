e7cac8bfb062ed1144eaa3d62de40253f7495838
jassim bahmida <jassim123bahmida@gmail.com>
2023-11-23 16:46:26 -0800

    0x13-more_singly_linked_lists

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,17 +1,20 @@
 #include "main.h"
 
 /**
- * get_endianness - checks the endianness of the system
+ * get_endianness - Checks the endianness of the system.
  *
- * Return: 0 if big endian, 1 if little endian
+ * Return:
+ *   If little-endian - 1
+ *   If big-endian - 0
  */
 int get_endianness(void)
 {
 	unsigned int num = 1;
-	char *endianCheck = (char *)&num;
 
-	if (*endianCheck)
-		return (1); /* Little Endian */
+	/* Use bitwise AND to check the least significant byte */
+	if (*(char *)&num)
+		return (1); /* System is little-endian */
 	else
-		return (0); /* Big Endian */
+		return (0); /* System is big-endian */
 }
+
