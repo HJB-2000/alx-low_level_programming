5863ed81d1d3c0fcd8d732df5958dc6c4b335401
HJB-2000 <jassim123bahmida@gmail.com>
2024-05-29 15:40:57 -0700

    0x1C-makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
-"""Defines an island perimeter measuring function."""
+"""Module to calculate the perimeter of an island in a grid."""
 
 
 def island_perimeter(grid):
-    """Return the perimiter of an island.
+    """
+    Calculate the perimeter of an island in a given grid.
 
-    The grid represents water by 0 and land by 1.
+    The grid is a list of lists where 0 represents water and 1 represents land.
 
     Args:
-        grid (list): A list of list of integers representing an island.
+        grid (list): A list of lists of integers representing the island.
+
     Returns:
-        The perimeter of the island defined in grid.
+        int: The perimeter of the island defined in the grid.
     """
     width = len(grid[0])
     height = len(grid)
     edges = 0
     size = 0
 
-    for i in range(height):
-        for j in range(width):
+    i = 0
+    while i < height:
+        j = 0
+        while j < width:
             if grid[i][j] == 1:
                 size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
+                if j > 0 and grid[i][j - 1] == 1:
                     edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
+                if i > 0 and grid[i - 1][j] == 1:
                     edges += 1
+            j += 1
+        i += 1
+
     return size * 4 - edges * 2
+
